2c194c39f0b56c90d34912baaff3f4547ddb4252
Jeremy Lam <jeremylam1995@gmail.com>
2024-12-12 14:59:50 +0000

    fix day 3 test

--- day_3/test_day_3.py
+++ day_3/test_day_3.py
@@ -3,4 +3,4 @@ from day_3 import extract_and_multiply
 def test_example_memory():
     test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     result = extract_and_multiply(test)
-    assert result == 161
+    assert result == 48
